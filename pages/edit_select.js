06d2645173a65fb91522a62536ffbf6ab9bd583b
nakamori akira <nakamori.1224@gmail.com>
2023-05-23 09:24:35 +0900

    ♻️ import Linkとimport Image の追加

--- pages/edit_select.js
+++ pages/edit_select.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import { Header } from "../components/Header";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,8 +26,20 @@ export default function Home() {
               {/* <SelectPc />
       <SelectSp /> */}
               <div className={styles.edit_select_list}>
+                <div className={styles.edit_select_number}>1</div>
+                <button className={styles.edit_select_btn}>
+                  <Link href="edit_page">
+                    <Image
+                      src="../images/edit_FILL0_wght400_GRAD0_opsz48.svg"
+                      alt="編集画像"
+                      width={48}
+                      height={48}
+                      priority
+                    />
+                  </Link>
+                </button>
                 <div
-                  className={`${styles.edit_select_item} ${styles.edit_select_name}`}
+                  className={`${styles.edit_select_item} ${styles.edit_select_coffee}`}
                 >
                   <label
                     htmlFor="name"
@@ -51,7 +65,7 @@ export default function Home() {
                   <div className={styles.edit_select_item_value}>７８％</div>
                 </div>
                 <div
-                  className={`${styles.edit_select_item} ${styles.edit_select_select_aroma}`}
+                  className={`${styles.edit_select_item} ${styles.edit_select_aroma}`}
                 >
                   <label
                     htmlFor="aroma"
@@ -150,9 +164,7 @@ export default function Home() {
                   </label>
                   <div className={styles.edit_select_item_value}>７点</div>
                 </div>
-                <div
-                  className={`${styles.edit_select_item} ${styles.edit_select_balance}`}
-                >
+                <div className={`${styles.edit_select_item} ${styles.balance}`}>
                   <label
                     htmlFor="balance"
                     className={styles.edit_select_item_title}
