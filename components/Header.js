839eb040f277f6cdff795e64bf464112e7afeb5e
nakamori akira <nakamori.1224@gmail.com>
2023-05-24 16:47:49 +0900

    ♻️ className属性の動的なクラス名付与を追加

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,38 @@
 import styles from "@/styles/Home.module.css";
+import React, { useState } from "react";
+// import Link from "next/link";
+
 export function Header() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.header_title}>
         <h1 className={styles.title}>Tasting Note</h1>
       </div>
       <div className={styles.header_button}>
-        <button className={styles.button}>
+        <button
+          className={
+            isActive
+              ? `${styles["button"]} ${styles["active"]}`
+              : styles["button"]
+          }
+          onClick={handleButtonClick}
+        >
           <span className={styles.button_item}></span>
           <span className={styles.button_item}></span>
           <span className={styles.button_item}></span>
         </button>
       </div>
-      <nav className={styles.menu}>
+      <nav
+        className={
+          isActive ? `${styles["menu"]} ${styles["active"]}` : styles["menu"]
+        }
+      >
         <ul className={styles.menu_list}>
           <li className={styles.menu_item}>New</li>
           <li className={styles.menu_item}>Delete</li>
