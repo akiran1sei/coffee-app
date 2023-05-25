fec9c4050b1094c48748f200fa5187fc1ce83aaf
nakamori akira <nakamori.1224@gmail.com>
2023-05-25 12:32:25 +0900

    ♻️ Linkプロパティ追加

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import styles from "@/styles/Home.module.css";
 import React, { useState } from "react";
-// import Link from "next/link";
+import Link from "next/link";
 
 export function Header() {
   const [isActive, setIsActive] = useState(false);
@@ -34,12 +34,29 @@ export function Header() {
         }
       >
         <ul className={styles.menu_list}>
+          <li className={styles.menu_item}>
+            {/* <div className={styles.keyboard}>
+              <div className={styles.keyboard_top}>Top</div>
+              <div className={styles.keyboard_front}></div>
+              <div className={styles.keyboard_left}></div>
+            </div> */}
+            <Link href={"/"}>Top</Link>
+          </li>
+          <li className={styles.menu_item}>
+            <Link href={"new_select"}>新規作成</Link>
+          </li>
+          <li className={styles.menu_item}>
+            <Link href={"edit_select"}>閲覧＆編集</Link>
+          </li>
+          <li className={styles.menu_item}>マイページ</li>
+        </ul>
+        {/* <ul className={styles.menu_list}>
           <li className={styles.menu_item}>New</li>
           <li className={styles.menu_item}>Delete</li>
           <li className={styles.menu_item}>保存</li>
           <li className={styles.menu_item}>管理</li>
           <li className={styles.menu_item}>編集</li>
-        </ul>
+        </ul> */}
       </nav>
     </header>
   );
